a30b71d6669e38ac69415d163707c6ad46f8e5ca
Zahra-Rahimii <zahra.rahimi.5908@gmail.com>
2025-08-14 11:53:11 +0330

    Minor changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 
 bootstrapApplication(AppComponent, {
