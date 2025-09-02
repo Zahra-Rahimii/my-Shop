9edeb0e0fb7d14a8f5051aae0c9a8cd594a6ba27
“HannahSanati” <hannahsanatiii@gmail.com>
2025-09-02 12:27:01 +0330

    updated style.css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.98.143:8080/api'
+  apiUrl: 'http://localhost:3000'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://192.168.98.143:8080/api'
+// };
